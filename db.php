5d1a3844fdb883abc850d4721afd8ec670979651
frankneck <finn.f1rst128@gmail.com>
2025-12-07 14:00:18 -0500

    Add db.php, edit info.php

---
+++ db.php
@@ -0,0 +1,42 @@
+<php?
+	$server="127.0.0.1"
+	$user="root"
+	$password="kali"
+	$dbName="first"
+
+	$link= mysqli_connect($server,$username,$password)
+	if (!link){
+		die("Error connect ". mysqli_connect_error());
+	}
+	
+	$sql = "CREATE DATABASE IF NOT EXIST $dbName";
+	if (!mysqli_query($link,$sql)){
+		echo "Error create DB". mysqli_error($link);
+	}
+
+	mysql_close($link);
+
+	$link = mysqli_connect($server, $username, $password, $dbName);
+	
+	$sql = "CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(15) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(20) NOT NULL"
+	
+	if (mysqli_query($link, $sql)){
+		echo "Error create table users". mysqli_error($link);
+	}
+
+	$sql = "CREATE TABLE IF NOT EXISTS posts(
+		id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		title VARCHAR(20) NOT NULL,
+		main_text VARCHAR(400) NOT NULL"
+	
+	if (mysqli_query($link, $sql)){
+		echo "Error create table users". mysqli_error($link);
+	}
+	
+	mysqli_close($link);
+?php>
+
